fffad139d70d737c738ccd1c972183644c792392
mzwendoda <95077324+mzwendoda@users.noreply.github.com>
2022-08-29 09:48:09 +0200

    icons added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< Updated upstream
 import { LoginComponent } from './login/login.component';
 
+=======
+import { SignupComponent } from './component/signup/signup.component';
+import { PostsComponent } from './component/posts/posts.component';
+import{LandingPageComponent} from './landing-page/landing-page.component';
+import {LoginComponent} from './component/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+>>>>>>> Stashed changes
 const routes: Routes = [
   {path:'',component: LoginComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+<<<<<<< Updated upstream
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 
+=======
+import { SignupComponent } from "./component/signup/signup.component";
+import { PostsComponent } from "./component/posts/posts.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import {MaterialModule} from './material/material.module';
+import { CarouselComponent } from './carousel/carousel.component';
+import { LoginComponent } from './component/login/login.component';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -18,12 +32,20 @@ import {MatCardModule} from '@angular/material/card';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    MatCardModule,
+    
     BrowserAnimationsModule,
+<<<<<<< Updated upstream
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule
+=======
+    ReactiveFormsModule,
+  
+    MatFormFieldModule,
+
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
