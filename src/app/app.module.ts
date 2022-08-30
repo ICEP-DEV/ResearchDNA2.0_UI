8aceb55db8b55e73a0f623c111e5bdaa4f572429
mzwendoda <95077324+mzwendoda@users.noreply.github.com>
2022-08-30 15:58:06 +0200

    profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< Updated upstream
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-
-=======
-import { SignupComponent } from "./component/signup/signup.component";
-import { PostsComponent } from "./component/posts/posts.component";
 import {ReactiveFormsModule} from "@angular/forms";
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import {MaterialModule} from './material/material.module';
-import { CarouselComponent } from './carousel/carousel.component';
-import { LoginComponent } from './component/login/login.component';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
->>>>>>> Stashed changes
+import { LoginComponent } from './login/login.component';
+import { MatLineModule } from '@angular/material/core';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @NgModule({
   declarations: [
@@ -33,19 +20,21 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     BrowserModule,
     MatCardModule,
+    MatLineModule,
+    MatDividerModule,
     
     BrowserAnimationsModule,
-<<<<<<< Updated upstream
+
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
-=======
+    MatCardModule,
+
     ReactiveFormsModule,
   
     MatFormFieldModule,
 
->>>>>>> Stashed changes
+
   ],
   providers: [],
   bootstrap: [AppComponent]
