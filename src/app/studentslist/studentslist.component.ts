f92d8b3c5a63de1303d452c72e14221b16d9ddb2
mzwendoda <95077324+mzwendoda@users.noreply.github.com>
2022-10-10 15:57:54 +0200

    studentlist

--- src/app/studentslist/studentslist.component.ts
+++ src/app/studentslist/studentslist.component.ts
@@ -1,6 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Studentlist } from '../studentlist.model';
 
+export interface Studentlists {
+  stud_number: string;
+  stud_name: string;
+  stud_surname: string;
+  Qualification: string;
+  Faculty:string;
+  proj_title:string
+}
+
+const STUDENT_DATA: Studentlists[] = [
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+  {stud_number: '215715213', stud_name: 'Siphesihle Pretty', stud_surname: 'Malinga', Qualification: 'Ndip:Software Development', Faculty:'ICT', proj_title:'Cyber Security'},
+];
+
 @Component({
   selector: 'app-studentslist',
   templateUrl: './studentslist.component.html',
@@ -8,105 +30,13 @@ import { Studentlist } from '../studentlist.model';
 })
 export class StudentslistComponent implements OnInit {
   
-
+  displayedColumns: string[] = ['stud_number', 'stud_name', 'stud_surname', 'Qualification','Faculty','proj_title'];
+  dataSource = STUDENT_DATA;
+  clickedRows = new Set<Studentlists>();
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
-  students: Studentlist[] = [
-    {
-      student_number:'215715213',
-    student_name: 'Siphesihle Pretty',
-    student_surname: 'Malinga',
-    Qualification: 'NDip: Software Development',
-        Faculty: 'ICT'
-    },
-    {
-      student_number:'212345780',
-      student_name: 'Samkelo Siya',
-    student_surname: 'Nkuna',
-    Qualification: 'NDip: Business Analyst',
-        Faculty: 'ICT'
-      },
-      {
-        student_number:'908222555',
-        student_name: 'Mpendulo Pet',
-        student_surname: 'Mathebula',
-        Qualification: 'NDip: Business Analyst',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'567211213',
-        student_name: 'Nontando Faith',
-        student_surname: 'Nkosi',
-        Qualification: 'NDip: Industrial Intelligence',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'213498333',
-        student_name: 'Angel Siphesihle',
-        student_surname: 'Malinga',
-        Qualification: 'NDip: Multi Media',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'215715213',
-        student_name: 'Yandiswa Prince',
-        student_surname: 'Shongwe',
-        Qualification: 'NDip: Software Development',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'215715213',
-        student_name: 'Minah Thabisile',
-        student_surname: 'Mathebula',
-        Qualification: 'NDip: Networking',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'215715213',
-        student_name: 'July Elliot',
-        student_surname: 'Malinga',
-        Qualification: 'NDip: Informatics',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'215715213',
-        student_name: 'Siphesihle Pretty',
-        student_surname: 'Malinga',
-        Qualification: 'NDip: Software Development',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'215715213',
-        student_name: 'Siphesihle Pretty',
-        student_surname: 'Malinga',
-        Qualification: 'NDip: Software Development',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'215715213',
-        student_name: 'Siphesihle Pretty',
-        student_surname: 'Malinga',
-        Qualification: 'NDip: Software Development',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'215715213',
-        student_name: 'Siphesihle Pretty',
-        student_surname: 'Malinga',
-        Qualification: 'NDip: Software Development',
-            Faculty: 'ICT'
-      },
-      {
-        student_number:'215715213',
-        student_name: 'Siphesihle Pretty',
-        student_surname: 'Malinga',
-        Qualification: 'NDip: Software Development',
-            Faculty: 'ICT'
-      },
-     
-      
-    ];
-
+  
 }
