9cab46167772db58506155f0137a4d4c9e982580
mzwendoda <95077324+mzwendoda@users.noreply.github.com>
2022-10-12 16:00:38 +0200

    v

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { EditComponent } from './edit/edit.component';
 import { LoginComponent } from './login/login.component';
 import { ResearchtoolsComponent } from './researchtools/researchtools.component';
 import { StudentslistComponent } from './studentslist/studentslist.component';
+import { SubmittedComponent } from './submitted/submitted.component';
+import { ViewprojectComponent } from './viewproject/viewproject.component';
 
 
 
@@ -15,7 +17,9 @@ const routes: Routes = [
   {path:'edit', component: EditComponent},
   {path:'researchtools', component:ResearchtoolsComponent},
   {path:'studentslist',component:StudentslistComponent},
-  {path:'admindashboard', component:AdmindashboardComponent}
+  {path:'admindashboard', component:AdmindashboardComponent},
+  {path:'viewproject', component:ViewprojectComponent},
+  {path:'submitted', component: SubmittedComponent}
 ];
 
 @NgModule({
